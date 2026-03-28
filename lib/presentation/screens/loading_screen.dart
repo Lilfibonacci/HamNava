2c23c71dd73abf4f098ab0cf05c3e09f70996794
T0WHIDM <towhidmgholami@gmail.com>
2026-03-28 22:42:48 +0330

    loading_screen_animation

--- lib/presentation/screens/loading_screen.dart
+++ lib/presentation/screens/loading_screen.dart
@@ -6,16 +6,49 @@ import 'package:go_router/go_router.dart';
 class LoadingScreen extends StatefulWidget {
   const LoadingScreen({super.key});
 
+  static String get routeName => 'loadingScreen';
+
   @override
   State<LoadingScreen> createState() => _LoadingScreenState();
-
-  static String get routeName => 'loadingScreen';
 }
 
-class _LoadingScreenState extends State<LoadingScreen> {
+class _LoadingScreenState extends State<LoadingScreen>
+    with TickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _fadeAnimation;
+  late Animation<double> _scaleAnimation;
+  late Animation<double> _pulseAnimation;
+
   @override
   void initState() {
     super.initState();
+
+    _controller = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 3),
+    );
+
+    _fadeAnimation = CurvedAnimation(
+      parent: _controller,
+      curve: const Interval(0, 1, curve: Curves.easeIn),
+    );
+
+    _scaleAnimation = Tween<double>(begin: 0.2, end: 1).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0, 0.6, curve: Curves.easeOutBack),
+      ),
+    );
+
+    _pulseAnimation = Tween<double>(begin: 0.9, end: 1.08).animate(
+      CurvedAnimation(
+        parent: _controller,
+        curve: const Interval(0.6, 1, curve: Curves.easeInOut),
+      ),
+    );
+
+    _controller.forward();
+
     Future.delayed(const Duration(seconds: 3), () {
       if (mounted) {
         context.goNamed(LoginScreen.namedRoute);
@@ -24,23 +57,45 @@ class _LoadingScreenState extends State<LoadingScreen> {
   }
 
   @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  Widget _logo() {
+    return FadeTransition(
+      opacity: _fadeAnimation,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: AnimatedBuilder(
+          animation: _pulseAnimation,
+          builder: (context, child) {
+            return Transform.scale(scale: _pulseAnimation.value, child: child);
+          },
+          child: SizedBox(
+            height: 300,
+            child: Image.asset('assets/images/hamnava.png'),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: SizedBox(
-              height: 300,
-              child: Image.asset('assets/images/hamnava.png'),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            _logo(),
+            const SpinKitPulsingGrid(
+              color: Color.fromARGB(255, 14, 208, 211),
+              size: 24,
             ),
-          ),
-          const SpinKitPulsingGrid(
-            color: Color.fromARGB(255, 14, 208, 211),
-            size: 32,
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
